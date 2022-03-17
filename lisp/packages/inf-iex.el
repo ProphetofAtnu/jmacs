74aafcf5193e16e000e2578a37db597dea61927f
Scaggs, Jacob <Jacob.Scaggs@alcoa.com>
2022-03-17 14:04:10 -0400

    Fixed inf-iex autocomplete

--- lisp/packages/inf-iex.el
+++ lisp/packages/inf-iex.el
@@ -1,185 +1,181 @@
-;;; -*- lexical-binding: t; -*-
+;;; inf-iex-compl.el ---                             -*- lexical-binding: t; -*-
 
-(defun inf-iex-mfa-autocomplete (prefix)
-  (let ((arg (reverse prefix)))
-    (format "Elixir.IEx.Autocomplete expand [\"%s\"]" arg)))
+(require 'dash)
 
-(defvar inf-iex-buffer-name " *erl_call*")
-(defvar inf-iex-repl-buffer-name "*iex*")
-(defvar inf-iex-process-name "iex")
+(defvar inf-iex-prompt-rx
+  "^iex([[:digit:]]+)> ")
 
-(defvar inf-iex--compl-proc nil)
-(defvar inf-iex-last-arg nil)
-(defvar inf-iex-current-arg nil)
-(defvar-local inf-iex-is-mix nil)
+(defvar inferior-iex-program "iex")
 
-(defvar inf-iex-stubs '())
-(defvar inf-iex-resultrx "{\\(?:yes\\|no\\), [\"\[]\\(.*\\)[\]\"], \\[\\(.*\\)\\]}")
+(defvar iex-redirect-buffer " *iex-completions*")
 
-(defvar inf-iex--cached-completions (make-hash-table
-                                     :test 'equal))
+(defconst inf-iex--redir-finished (rx
+                                   (group (1+ ?\b))
+                                   (1+ space)
+                                   (backref 1)))
 
-(defun inf-iex--detect-mix-project ()
-  (cl-do ((mix-dir nil)
-          (pth (reverse (split-string (expand-file-name ".") "/"))
-               (cdr pth)))
-         ((null pth) nil)
-    (let ((dn (string-join (reverse pth) "/")))
-      (when (member "mix.exs" (directory-files dn))
-        (cl-return dn)))))
+(defvar-local *inf-iex--last-prefix* nil)
+(defvar-local *inf-iex--last-result* nil)
+(defvar-local *inf-iex--top-level-mods* nil)
 
-(define-derived-mode
-    inf-iex-mode comint-mode "IEx"
-    (add-hook 'completion-at-point-functions
-              'inf-iex-completion-at-point 0 t))
+(defvar-local *iex-proc* nil)
 
-(defun inf-iex-run ()
-  (interactive)
-  (with-current-buffer (get-buffer-create inf-iex-repl-buffer-name)
-    (unless (process-live-p (get-buffer-process (current-buffer)))
-      (inf-iex-mode)
-      (make-comint-in-buffer inf-iex-process-name inf-iex-repl-buffer-name "iex" nil
-                             "--sname" "iiex"))
-    (display-buffer (current-buffer))))
+(defun inf-iex-collect-input ()
+  (funcall comint-get-old-input))
 
-(defun inf-iex-mix-run ()
-  (interactive)
-  (when-let ((d (inf-iex--detect-mix-project)))
-    (with-current-buffer (get-buffer-create inf-iex-repl-buffer-name)
-      (cd d)
-      (unless (process-live-p (get-buffer-process (current-buffer)))
-        (inf-iex-mode)
-        (make-comint-in-buffer inf-iex-process-name inf-iex-repl-buffer-name "iex" nil
-                               "--sname" "iiex" "-S" "mix")
-        (setq-local inf-iex-is-mix t))
-      (display-buffer (current-buffer)))))
+(defun inf-iex--read-completions-buffer ()
+  (when-let ((buf (get-buffer iex-redirect-buffer)))
+    (with-current-buffer buf
+      (save-excursion
+        (let ((eofl (progn (goto-char (point-min))
+                           (point-at-eol))))
+          (goto-char (point-max))
+          (split-string
+           (buffer-substring-no-properties
+            eofl
+            (point-at-bol))
+           "[[:space:]]" t))))))
 
-(defun inf-iex-recompile ()
-  (interactive)
-  (comint-send-string (get-process inf-iex-process-name) "recompile()\n"))
+(defun inf-iex--propertize-completion-item (item)
+  (let* 
+      ((items (split-string item "/"))
+       (compl (car items))
+       (md (cdr-safe items)))
+    (propertize compl
+                :arity (car md))))
 
-(defun inf-iex--ac-filter-builder (stub)
-  (lambda (proc dat)
-    (with-current-buffer
-        (process-buffer proc)
-      (erase-buffer)
-      (when (string-match
-             inf-iex-resultrx
-             dat)
-        (when (length>
-               (match-string 1 dat)
-               0)
-          (insert (concat stub (match-string 1 dat)))
-          (newline))
-        (let ((r "\"\\([^\",]+\\)\"")
-              (cand (match-string 2 dat))
-              (idx 0))
-          (while (setf idx
-                       (and (length> cand idx)
-                            (string-match r cand idx)))
-            (insert (match-string 1 cand))
-            (newline)
-            (cl-incf idx)))))))
+(defun inf-iex--propertize-completions (comps)
+  (mapcar #'inf-iex--propertize-completion-item comps))
 
-(defun inf-iex--do-ac (arg &optional append)
-  (with-current-buffer (get-buffer-create inf-iex-buffer-name)
-    (unless (and inf-iex--compl-proc
-                 (process-live-p inf-iex--compl-proc))
-      (setq inf-iex--compl-proc
-            (make-process
-             :name "erl_call"
-             :buffer inf-iex-buffer-name
-             :filter  (inf-iex--ac-filter-builder (or append arg))
-             :sentinel '(lambda (p s) nil)
-             :command (list
-                       "erl_call"
-                       "-a"
-                       (inf-iex-mfa-autocomplete arg)
-                       "-sname"
-                       "iiex@localhost")))))
-  inf-iex--compl-proc)
+(defun inf-iex--retrieve-completions ()
+  (inf-iex--propertize-completions
+   (inf-iex--read-completions-buffer)))
 
-(defun inf-iex--read-result () 
-  (with-current-buffer (get-buffer inf-iex-buffer-name)
-    (goto-char (point-min))
-    (let ((lns (list
-                (buffer-substring-no-properties
-                 (point-at-bol)
-                 (point-at-eol)))))
-      (while (line-move 1 t)
-        (let ((lcontent (buffer-substring-no-properties
-                         (point-at-bol)
-                         (point-at-eol))))
-          (when (length> lcontent 0)
-            (push lcontent
-                  lns))))
-      (nreverse lns))))
+(defun inf-iex-get-native-completions (process stub)
+  "Natively complete elixir statement `stub' by querying the IEx
+process, `process'"
+  (with-current-buffer (process-buffer process)
+    (let* ((original-filter-fn (process-filter process))
+           (redirect-buffer (get-buffer-create
+                             iex-redirect-buffer))
+           (trigger "\x9")
+           (clear-repl "\x01\x0b")
+           (current-input (if (string= "" stub)
+                              " "
+                            stub))
+           (done nil))
+      (with-current-buffer redirect-buffer
+        (erase-buffer)
+        (process-send-string process (concat current-input trigger clear-repl))
+        (set-process-filter process
+                            #'(lambda (p str)
+                                (with-current-buffer redirect-buffer
+                                  (save-excursion
+                                    (goto-char (point-max))
+                                    (insert str)
+                                    (when (looking-back inf-iex--redir-finished)
+                                      (setf done t)
+                                      (set-process-filter p original-filter-fn))))))
+        (while (not done)
+          (accept-process-output process 1 0 t)
+          )
+        (inf-iex--retrieve-completions)))))
 
-(defun inf-iex--to-capf-fmt (comps)
-  (mapcar
-   #'(lambda (x)
-     (cl-destructuring-bind (sym . ar)
-         (split-string x "/")
-         (if-let ((arity (car-safe ar)))
-             (propertize sym 'arity arity)
-           sym)))
-   comps))
+(defun inf-iex--get-completion-range ()
+  (save-excursion
+    (reverse (list (point)
+                   (progn
+                     (skip-syntax-backward "\w_")
+                     (point))
+                   (progn
+                     (skip-syntax-backward
+                      "w_.")
+                     (point))))))
 
-(defun inf-iex--annot-func (x)
-  (when-let
-      ((prop
-        (plist-get
-         (text-properties-at
-          0 x)
-         'arity)))
-    (format "/%s" prop)))
+(defun inf-iex--complete-mods-tl (proc &rest ignored)
+  (if *inf-iex--top-level-mods*
+      *inf-iex--top-level-mods*
+    (let ((res
+           (inf-iex-get-native-completions proc "Elixir.")))
+      (setq *inf-iex--top-level-mods* res)
+      res)))
 
-(defun inf-iex--point-region ()
-    (save-excursion
-    (let ((e (point))
-          (ls (skip-syntax-backward "^.-"))
-          (s (skip-syntax-backward "w_.")))
-      (list (+ e ls s) (+ e ls) e))))
+(defun inf-iex--annot-func (item)
+  (if-let ((prop (get-text-property 0 :arity item)))
+      (format "/%s" prop)
+    " -> mod"))
 
-(defun inf-iex--extract-module (part)
-    (string-trim-right part "\\.?[^\\.]*$"))
-
-(defun inf-iex--cache-completions (query results)
-  (let* ((mod (inf-iex--extract-module query))
-         (cur (gethash mod inf-iex--cached-completions nil)))
-    (dolist (x results)
-      (unless (member x cur)
-        (push x cur)))
-    (puthash mod cur inf-iex--cached-completions)))
-
-(defun inf-iex--get-cached-completions (st sep end)
-  (lambda (_) 
-    (let* ((ac (buffer-substring-no-properties st end))
-           (mod (inf-iex--extract-module ac)))
-      (gethash mod inf-iex--cached-completions nil))))
-
-(defun inf-iex--get-completions (st sep end)
-  (let* ((ac (buffer-substring-no-properties st end))
-         ;; completion region
-         (r (buffer-substring-no-properties sep end))
-         (proc (inf-iex--do-ac ac (unless (string-empty-p r)
-                                    r))))
-    (lambda (prefix)
-      (when (waiting-for-user-input-p)
-        (accept-process-output proc nil 100 t))
-      (let ((dat (inf-iex--read-result)))
-        (when (length> dat 0)
-          (let ((fmt (inf-iex--to-capf-fmt dat)))
-            (run-with-idle-timer 1 nil 'inf-iex--cache-completions ac fmt)
-            fmt))))))
+(defun inf-iex--completion-function (proc prefix item &rest ignored)
+  (if (and *inf-iex--last-result*
+           (string= *inf-iex--last-prefix*
+                    prefix))
+      *inf-iex--last-result*
+    (let ((res
+           (inf-iex-get-native-completions proc prefix)))
+      (setq *inf-iex--last-prefix* prefix)
+      (setq *inf-iex--last-result* res)
+      res)))
 
 (defun inf-iex-completion-at-point ()
-  (cl-destructuring-bind (st sep end) (inf-iex--point-region)
-    (list
-     sep end
-     (completion-table-merge
-      (completion-table-dynamic (inf-iex--get-cached-completions st sep end))
-      (completion-table-with-cache (inf-iex--get-completions st sep end)))
-     :annotation-function 'inf-iex--annot-func)))
+  "Completion at point function for inferior iex process"
+  (-let* (((expr start end) (inf-iex--get-completion-range))
+          (item (buffer-substring-no-properties start end))
+          (prefix (buffer-substring-no-properties expr start))
+          (base-ctable
+           (completion-table-dynamic
+            (apply-partially
+             #'inf-iex--completion-function
+             (get-buffer-process (current-buffer))
+             prefix item))))
+    (unless (syntax-ppss-context (syntax-ppss))
+      (list start end
+            (cond
+              ((string= prefix "")
+               (completion-table-merge
+                base-ctable
+                (completion-table-dynamic
+                 (apply-partially #'inf-iex--complete-mods-tl
+                                  (get-buffer-process (current-buffer))))))
+              (t base-ctable))
+            :annotation-function #'inf-iex--annot-func))))
+
+(define-derived-mode inferior-iex-mode
+    comint-mode "Inferior IEx"
+    (add-hook 'completion-at-point-functions
+              #'inf-iex-completion-at-point nil 'local))
+
+(cl-defun create-iex-buffer (&key
+                               (buffer "*iex*")
+                               (exec "iex")
+                               (proc-name "iex")
+                               (args nil))
+  (let ((comint-terminfo-terminal "vt100"))
+    (with-current-buffer (apply #'make-comint-in-buffer
+                                proc-name buffer exec nil args)
+      (inferior-iex-mode)
+      (setq *iex-proc*
+            (get-buffer-process
+             (current-buffer)))
+      (current-buffer))))
+
+(defun run-iex (&rest args)
+  (interactive)
+  (display-buffer
+   (create-iex-buffer
+    :exec inferior-iex-program)))
+
+(defun run-iex-with-mix ()
+  (interactive)
+  (display-buffer
+   (create-iex-buffer
+    :buffer "*iex-mix*"
+    :proc-name "iex-mix"
+    :exec inferior-iex-program
+    :args '("-S" "mix"))))
+
+(defun inf-iex--get-help (proc str)
+  (process-send-string
+   proc
+   (format "h %s\n" str)))
 
 (provide 'inf-iex)
