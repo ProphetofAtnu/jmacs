7636c5c1d7fad82b422c4404bee8a17ce56ecdc5
Jacob Scaggs <jacscaggs@gmail.com>
2023-09-28 16:12:30 -0400

    Removed benchmarking from straight boostrap file.

--- lisp/early/bootstrap-straight.el
+++ lisp/early/bootstrap-straight.el
@@ -2,19 +2,18 @@
 
 (defvar straight-check-for-modifications '(check-on-save find-when-checking))
 
-(benchmark-progn 
-  (defvar bootstrap-version)
-  (let ((bootstrap-file
-	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-	(bootstrap-version 6))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-	(goto-char (point-max))
-	(eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage)))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;; Included for convenience
 ;; (straight-use-package 'use-package)
