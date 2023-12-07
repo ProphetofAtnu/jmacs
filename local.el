a92102cee433b6210b30c19192b4f49fd501253e
Jacob Scaggs <jacscaggs@gmail.com>
2023-12-07 17:47:19 -0500

    Made local.el a customization point

--- local.el
+++
@@ -1,6 +0,0 @@
-;;; -*- lexical-binding: t; -*-
-
-(setq lsp-mssql-connections
-      [(:server "MAS-DBWEST-SQL.apm.alcoa.com"
-                :database "CC"
-                :authenticationType "Integrated")])
